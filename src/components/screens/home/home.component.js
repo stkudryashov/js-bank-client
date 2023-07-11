6de9368575a18b0e07caea59b4a6a049958c5456
Aleks Kudryashov <aleks59niki30@gmail.com>
2023-07-11 15:48:10 +0400

    update screens

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -7,6 +7,7 @@ import styles from './home.module.scss'
 import { $R } from '@/core/rquery/rquery.lib'
 import { Button } from '@/components/ui/button/button.component'
 import { Field } from '@/components/ui/field/field.component'
+import { UserItem } from '@/components/ui/user-item/user-item.component'
 
 export class Home extends BaseScreen {
   constructor() {
@@ -25,7 +26,15 @@ export class Home extends BaseScreen {
         name: 'name',
         variant: 'credit-card',
         type: 'text'
-      })
+      }),
+      new UserItem(
+        {
+          avatarPath: 'https://cdn-icons-png.flaticon.com/512/1946/1946429.png',
+          name: 'Kudryashov'
+        },
+        false,
+        () => alert('hey')
+      )
     ])
 
     $R(element).find('p').css('color', 'red')
