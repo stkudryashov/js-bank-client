03cb23bd313dc3cb896fe6a5395e13df7b63255e
Aleks Kudryashov <aleks59niki30@gmail.com>
2023-07-11 22:30:20 +0400

    entrypoint modify

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import '@/styles/global.scss'
 
 import { Router } from './core/router/router'
+import { devMode } from './config/dev.config'
+
+import { notify } from './core/services/notification.service'
+import { storageService } from './core/services/storage.service'
+
+if (devMode) {
+  window.notify = notify
+  window.storageService = storageService()
+}
 
 new Router()
